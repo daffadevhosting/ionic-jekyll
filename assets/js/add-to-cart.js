9d8c1ad741b394074fc3b0161547858d794992b2
“daffadev-inc” <daffadev.inc@gmail.com>
2022-04-15 23:53:11 +0700

    update

--- assets/js/add-to-cart.js
+++
@@ -1,9 +0,0 @@
-jQuery('#item_add').click(function() {
-
-  $('html, body').animate({
-    scrollTop: 0
-  }, 100);
-
-  $('#cart-menu').addClass('animated shake');
-
-});
--- assets/js/style-picker.js
+++
@@ -1,5 +0,0 @@
-jQuery('.style-picker div').click(function() {
-  var target = $(this).attr('id');
-  $(this).addClass('item_color').siblings().removeClass('item_color');
-  $('#' + target).show().siblings('div').hide();
-});

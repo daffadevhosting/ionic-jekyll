47558d13de2208a928c7e4bfa15a81eb3306f5bb
“daffadev-inc” <daffadev.inc@gmail.com>
2022-06-09 22:23:19 +0700

    update

---
+++ _site/sw.js
@@ -0,0 +1,46 @@
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js');
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, StaleWhileRevalidate } = workbox.strategies;
+const { CacheableResponse } = workbox.cacheableResponse;
+
+workbox.core.setCacheNameDetails({
+  prefix: 'http://localhost:4000',
+  suffix: '2022-06'
+});
+
+registerRoute(
+  '/',
+  new NetworkFirst()
+);
+
+registerRoute(
+  /page[0-99]/,
+  new NetworkFirst()
+)
+
+registerRoute(
+  new RegExp('/\\d{4}/\\d{2}/\\d{2}/.+'),
+  new StaleWhileRevalidate()
+)
+
+workbox.precaching.precacheAndRoute([
+  { url: '/', revision: '202206092221' },
+  { url: '/product', revision: '202206092221' },
+  { url: '/assets/css/style.css', revision: '202206092221' },
+  { url: '/assets/css/webshop.css', revision: '202206092221' }
+])
+
+registerRoute(
+  ({request}) => request.destination === 'image' ,
+  new CacheFirst({
+    plugins: [
+      new CacheableResponse({statuses: [0, 200]})
+    ],
+  })
+);
+
+registerRoute(
+  /\/(images|icon|css)/,
+  new CacheFirst()
+);
---
+++ sw.js
@@ -0,0 +1,52 @@
+---
+layout: none
+---
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js');
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, StaleWhileRevalidate } = workbox.strategies;
+const { CacheableResponse } = workbox.cacheableResponse;
+
+workbox.core.setCacheNameDetails({
+  prefix: '{{site.url}}',
+  suffix: '{{ site.time | date: "%Y-%m" }}'
+});
+
+registerRoute(
+  '/',
+  new NetworkFirst()
+);
+
+registerRoute(
+  /page[0-99]/,
+  new NetworkFirst()
+)
+
+registerRoute(
+  new RegExp('/\\d{4}/\\d{2}/\\d{2}/.+'),
+  new StaleWhileRevalidate()
+)
+
+workbox.precaching.precacheAndRoute([
+  {% for post in site.posts limit:14 -%}
+  { url: '{{ post.url }}', revision: '{{ post.date | date: "%Y-%m-%d"}}' },
+  {% endfor -%}
+  { url: '/', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
+  { url: '/product', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
+  { url: '/assets/css/style.css', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
+  { url: '/assets/css/webshop.css', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' }
+])
+
+registerRoute(
+  ({request}) => request.destination === 'image' ,
+  new CacheFirst({
+    plugins: [
+      new CacheableResponse({statuses: [0, 200]})
+    ],
+  })
+);
+
+registerRoute(
+  /\/(images|icon|css)/,
+  new CacheFirst()
+);

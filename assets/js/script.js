019650bc6f95d067c11765acafc46574795bd753
“daffadev-inc” <daffadev.inc@gmail.com>
2022-06-13 09:49:54 +0700

    update

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Use matchMedia to check the user preference
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+toggleDarkTheme(prefersDark.matches);
+
+// Listen for changes to the prefers-color-scheme media query
+prefersDark.addListener((mediaQuery) => toggleDarkTheme(mediaQuery.matches));
+
+// Add or remove the "dark" class based on if the media query matches
+function toggleDarkTheme(shouldAdd) {
+  document.body.classList.toggle('dark', shouldAdd);
+}
